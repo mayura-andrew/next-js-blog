13f4a72ea4ebcfb1df6b885f613360f81c026600
mayura_andrew <mayura.a@parallax.lk>
2023-02-22 20:52:04 +0530

    typescript

--- lib/posts.ts
+++ lib/posts.ts
@@ -25,7 +25,7 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data,
+      ...(matterResult.data as { date: string; title: string})
     };
   });
   // Sort posts by date
@@ -66,7 +66,7 @@ export function getAllPostIds() {
   
 
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
 
@@ -81,6 +81,6 @@ export async function getPostData(id) {
     return {
         id, 
         contentHtml,
-        ...matterResult.data
+        ...(matterResult.data as {date: string; title: string})
     }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,12 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+import { GetStaticProps } from 'next';
+
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
   return {
     props: {
       allPostsData
@@ -13,16 +17,24 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Mayura Andrew. I'm a open-source software engineer.</p>
+        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -31,18 +43,15 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>{title}</Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          
-         
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
-
     </Layout>
-  );
+  )
 }
